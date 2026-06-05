98d51332b83079138d3137d8d1e61c8fd947dc96
Raphael Simões Andrade - Mídia 3 <138605545+RaphaelAndradeM3@users.noreply.github.com>
2026-06-04 22:45:19 -0300

    Iniciar_AdminWeb.bat

---
+++ Iniciar_AdminWeb.bat
@@ -0,0 +1,11 @@
+@echo off
+title Project Uploader - Admin WebApp
+echo ==============================================
+echo Iniciando Painel Administrativo Web...
+echo ==============================================
+echo.
+
+cd src\ProjectUploader.WebApp
+npm run dev
+
+pause
